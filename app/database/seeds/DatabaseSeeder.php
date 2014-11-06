e72a52eb24e3e7dd4e46e801657bb2a22f3ec255
Mauricio Trajano <mauriciot1993@gmail.com>
2014-11-06 17:42:45 -0500

    user table seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-class DatabaseSeeder extends Seeder {
-
+class DatabaseSeeder extends Seeder 
+{
 	/**
 	 * Run the database seeds.
 	 *
@@ -11,7 +11,34 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
 	}
+}
 
+class UserTableSeeder extends Seeder
+{
+	public function run()
+	{
+		if(($fp = fopen(base_path() . '/data/users.csv', 'r')) !== false){
+            fgetcsv($fp); //ignore first line
+			while(($arr = fgetcsv($fp)) !== false){
+				User::create([
+					'f_name' => $arr[1],
+					'l_name' => $arr[2],
+					'b_day' => $arr[3],
+                    'gender' => $arr[0],
+					'email' => $arr[4],
+					'password' => $arr[5],
+					'address' => $arr[6],
+					'city' => $arr[7],
+					'state' => $arr[8],
+					'zip' => $arr[9],
+					'phone' => $arr[10],
+				]);
+			}
+		}
+		else{
+			echo "Problem opening file";
+		}
+	}
 }
