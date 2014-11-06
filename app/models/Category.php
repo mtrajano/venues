8f611c38b5f7f0769e5a4256a32cd921a5f96aef
Mauricio Trajano <mauriciot1993@gmail.com>
2014-11-05 22:38:39 -0500

    models/relationships completed

--- app/models/Category.php
+++ app/models/Category.php
@@ -5,4 +5,9 @@ class Category extends Eloquent
 	protected $table = 'categories';
 
 	protected $fillable = array('name', 'description');
+
+	public function topics()
+	{
+		return $this->belongsTo('Topic');
+	}
 }
--- app/models/Venue.php
+++ app/models/Venue.php
@@ -5,4 +5,9 @@ class Venue extends Eloquent
 	protected $table = 'venues';
 
 	protected $fillable = array('name', 'address', 'zip', 'phone');
+
+	public function hosted()
+	{
+		return $this->belongsTo('Event');
+	}
 }
