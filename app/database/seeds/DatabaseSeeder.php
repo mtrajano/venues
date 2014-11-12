0c584386b2207e42db595ef48d6d8b8ae3e0a9d3
LucianoTaranto <lucianomtaranto@gmail.com>
2014-11-12 03:19:57 -0500

    Created and added data for Attends/Events/Organizers/Venues in the data/ folder. Updated DatabaseSeeder to populate database with this new data

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -62,6 +62,73 @@ class TopicTableSeeder extends Seeder
 	}
 }
 
+class AttendTableSeeder extends Seeder
+{
+	public function run()
+	{
+		if(($fp = fopen(base_path() . '/data/attends.csv', 'r')) !== false){
+            fgetcsv($fp); //ignore first line
+			while(($arr = fgetcsv($fp)) !== false){
+				Attend::create([
+					'user_id' => $arr[0],
+					'event_id' => $arr[1]
+				]);
+			}
+		}
+	}
+}
+
+class OrganizerTableSeeder extends Seeder
+{
+	public function run()
+	{
+		if(($fp = fopen(base_path() . '/data/organizers.csv', 'r')) !== false){
+            fgetcsv($fp); //ignore first line
+			while(($arr = fgetcsv($fp)) !== false){
+				Organizer::create([
+					'name' => $arr[0],
+					'email' => $arr[1]
+				]);
+			}
+		}
+	}
+}
+
+class VenueTableSeeder extends Seeder
+{
+	public function run()
+	{
+		if(($fp = fopen(base_path() . '/data/venues.csv', 'r')) !== false){
+            fgetcsv($fp); //ignore first line
+			while(($arr = fgetcsv($fp)) !== false){
+				Venue::create([
+					'name' => $arr[0],
+					'address' => $arr[1],
+					'zip' => $arr[2]
+				]);
+			}
+		}
+	}
+}
+
+class EventTableSeeder extends Seeder
+{
+	public function run()
+	{
+		if(($fp = fopen(base_path() . '/data/events.csv', 'r')) !== false){
+            fgetcsv($fp); //ignore first line
+			while(($arr = fgetcsv($fp)) !== false){
+				Event::create([
+					'when' => $arr[0],
+					'name' => $arr[1],
+					'topic_id' => $arr[2],
+					'venue_id' => $arr[3]
+				]);
+			}
+		}
+	}
+}
+
 class CategoryTableSeeder extends Seeder
 {
 	public function run()
