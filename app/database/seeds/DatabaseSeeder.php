be21427e483c68cc8b0593dff076e28b5ed20aa2
Mauricio Trajano <mauriciot1993@gmail.com>
2014-11-30 23:13:06 -0500

    all seeds except likes and attends

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
 
 		// $this->call('UserTableSeeder');
 		$this->call('TestUserTableSeeder');
+		$this->call('GenreTableSeeder');
 		$this->call('ArtistTableSeeder');
+		$this->call('VenueTableSeeder');
+		$this->call('ShowTableSeeder');
 	}
 }
 
@@ -82,25 +85,82 @@ class TestUserTableSeeder extends Seeder
 	}
 }
 
+class GenreTableSeeder extends Seeder
+{
+	public function run()
+	{
+		Genre::create(['name' => 'No Genre']);
+	}
+}
+
 class ArtistTableSeeder extends Seeder
 {
 	public function run()
 	{
+		//read in json format
+		$input = file_get_contents(base_path() . '/data/artists.json');
+		$json = json_decode($input);
+
+		foreach($json as $json_obj) {
+			Artist::create([
+				'name' => $json_obj->name,
+				'jambase_id' => $json_obj->jambase_id,
+				'number_likes' => str_replace( ',', '', $json_obj->number_fans)
+			]);
+		}
+
+		$input = file_get_contents(base_path() . '/data/genres.json');
+		$json = json_decode($input);
+
+		foreach($json as $name=>$genre) {
+			if(!$genre){
+				continue;
+			}
+
+			Artist::where('name', $name)->update(['genre_id' => Genre::firstOrCreate(['name' => $genre])->id]);
+		}
+	}
+}
+
+class VenueTableSeeder extends Seeder
+{
+	public function run()
+	{
+		$input = file_get_contents(base_path() . '/data/venues.json');
+		$json = json_decode($input);
+
+		foreach($json as $json_obj){
+			Venue::create([
+				'name' => $json_obj->name,
+				'address' => $json_obj->street,
+				'city' => $json_obj->city,
+				'state' => $json_obj->state,
+				'zip' => $json_obj->zipcode,
+				'jambase_id' => $json_obj->jambase_id
+			]);
+		}
+
+		//malformatted data
+		Venue::where('name', 'The Monterey Club')->update(['city' => 'Fort Lauderdale', 'state' => 'FL', 'zip' => '33316'])
+    }
+}
+
+class ShowTableSeeder extends Seeder
+{
+	public function run()
+	{
         //read in json format
-        $input = file_get_contents(base_path() . '/data/artists.json');
+        $input = file_get_contents(base_path() . '/data/shows.json');
         $json = json_decode($input);
 
         foreach($json as $json_obj){
-            if (! $json_obj->genre) {
-                continue;
-            }
-
-            Artist::create([
-                'name' => $json_obj->name,
-                'genre_id' => Genre::firstOrCreate(['name' => $json_obj->genre])->id,
-                'artistlink_id' => $json_obj->id,
+            Show::create([
+                'when' => $json_obj->date,
+                'artist_id' => Artist::firstOrCreate(['name' => $json_obj->artist])->id,
+                'venue_id' => Venue::firstOrCreate(['name' => $json_obj->venue])
             ]);
         }
 	}
 }
+
 
