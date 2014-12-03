dd02cc7ed736560b20339d83250efa0bb4faf9b9
Mauricio Trajano <mauriciot1993@gmail.com>
2014-12-02 22:12:38 -0500

    all plots completed

--- app/controllers/admin/AdminDataController.php
+++ app/controllers/admin/AdminDataController.php
@@ -30,6 +30,45 @@ END_QUERY;
 
 		Cache::forever('profit_data', $profit_data);
 
+$query = <<<END_QUERY
+SELECT AVG(num_sales) AS avg_sale, price 
+FROM tickets 
+GROUP BY price
+ORDER BY price
+END_QUERY;
+
+		$price_sales = json_encode(DB::select(DB::raw($query)));
+
+		Cache::forever('price_sales', $price_sales);
+
+$query = <<<END_QUERY
+SELECT
+like_ranges.like_range AS number_likes,
+AVG(tickets.num_sales) AS avg_sale
+FROM (
+  SELECT id,
+     CASE
+         WHEN number_likes = 0 THEN'0'
+         WHEN number_likes > 0 AND number_likes <= 20 THEN '1-20'
+         WHEN number_likes > 20 AND number_likes <= 100 THEN '20-100'
+         WHEN number_likes > 100 AND number_likes <= 500 THEN '100-500'
+         WHEN number_likes > 500 AND number_likes <= 1000 THEN '500-1000'
+         WHEN number_likes > 1000 AND number_likes <= 5000 THEN '1000-5000'
+         ELSE '5000+'
+    END AS like_range
+ FROM artists) like_ranges
+INNER JOIN shows
+ON like_ranges.id = shows.artist_id
+INNER JOIN tickets
+ON shows.id = tickets.show_id
+GROUP BY number_likes
+ORDER BY FIELD (number_likes, '0', '1-20', '20-100', '100-500', '500-1000','1000-5000', '5000+');
+END_QUERY;
+
+		$likes_sales = json_encode(DB::select(DB::raw($query)));
+
+		Cache::forever('likes_sales', $likes_sales);
+
 		return View::make('admin.data');
 	}
 }
--- app/views/admin/data.blade.php
+++ app/views/admin/data.blade.php
@@ -30,6 +30,30 @@
     	    </div>
 	    </div>
 	</div>
+	<div class="row">
+	    <div class="col-lg-12">
+	    	<div class="panel panel-default">
+	    	    <div class="panel-heading">
+	    	    	<h3 class="panel-title">Number of Likes vs Number of Sales</h3>
+	    	    </div>
+	    	    <div class="panel-body">
+	    	    	<div id="chartContainer2"></div>
+	    	    </div>
+    	    </div>
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="col-lg-12">
+	    	<div class="panel panel-default">
+	    	    <div class="panel-heading">
+	    	    	<h3 class="panel-title">Price of Ticket vs Number of Sales</h3>
+	    	    </div>
+	    	    <div class="panel-body">
+	    	    	<div id="chartContainer3"></div>
+	    	    </div>
+    	    </div>
+	    </div>
+	</div>
 </div>
 
 @stop
@@ -39,6 +63,7 @@
 <script>
 
 $(function(){
+	//Graph 1 - Number of likes of artist and price of ticket vs the profits made in concert
 	data = {{ Cache::get('profit_data') }};
 
 	var svg = dimple.newSvg("#chartContainer", 590, 400);
@@ -51,6 +76,38 @@ $(function(){
 	chart.addSeries(["number_likes, price"], dimple.plot.bubble);
 	chart.addLegend(180, 10, 360, 20, "right");
 	chart.draw();
+
+	//Graph 2 - Price of tickets vs the number of ticket sales
+	new Morris.Line({
+	  // ID of the element in which to draw the chart.
+	  element: 'chartContainer2',
+	  // Chart data records -- each entry in this array corresponds to a point on
+	  // the chart.
+	  data: {{ Cache::get('price_sales') }},
+	  // The name of the data record attribute that contains x-values.
+	  xkey: 'price',
+	  // A list of names of data record attributes that contain y-values.
+	  ykeys: ['avg_sale'],
+	  // Labels for the ykeys -- will be displayed when you hover over the
+	  // chart.
+	  labels: ['Average Sales']
+	});
+
+	//Graph 3 - Number of likes of artist vs the number of ticket sales
+	new Morris.Line({
+	  // ID of the element in which to draw the chart.
+	  element: 'chartContainer3',
+	  // Chart data records -- each entry in this array corresponds to a point on
+	  // the chart.
+	  data: {{ Cache::get('likes_sales') }},
+	  // The name of the data record attribute that contains x-values.
+	  xkey: 'number_likes',
+	  // A list of names of data record attributes that contain y-values.
+	  ykeys: ['avg_sale'],
+	  // Labels for the ykeys -- will be displayed when you hover over the
+	  // chart.
+	  labels: ['Average Sales']
+	});
 });
 
 </script>
