87bb6c49cd45078521af0e4daa4a2e4d274c1e68
Mauricio Trajano <mauriciot1993@gmail.com>
2014-12-04 11:34:39 -0500

    removed front page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,17 +10,7 @@
 			</div>
 			<div id="search-panel" class="panel-body">
 			  	<div id="search-container">
-			  		<p id="search-desc">Click on search by event to search for events around your area, or type in a name and click on search by friends to see what your friends are up to!</p>
-					<div class="input-group">
-						<input type="text" class="form-control">
-						<span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
-					</div>
-					<nav>
-						<ul class="pagination menu">
-				  			<li><a href="#/eventfinder">Search By Event</a></li>
-				  			<li><a href="#/friendsfinder">Search By Friends</a></li>
-						</ul>
-					</nav>
+			  		<p id="search-desc">Sign in to get started! Click on Users to see friends around you that are looking for music just like you. Click on Artists to see all the artists we have available or top 100 to see the most liked artists. Finally you can click on Events to see the upcoming events.</p>
 			  	</div>
 			</div>
 		</div>
